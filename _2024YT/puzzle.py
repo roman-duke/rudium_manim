5b6cedeb7e8f5337f8255bd20697d9618f6996a2
Donald Roman <romanunuose@gmail.com>
2024-11-30 23:49:30 +0100

    Testing the SVGMobject class by animating the creation of a complex train to be used in the video

--- _2024YT/puzzle.py
+++ _2024YT/puzzle.py
@@ -2,6 +2,14 @@ from manim import *
 
 class Puzzle(Scene):
   def construct(self):
-    gnuSVG = SVGMobject('./_2024YT/fly_2trains/assets/manim_svg_test.svg')
+    # Animate the creation of a complex train
+    complexTrainSVG = SVGMobject('./_2024YT/fly_2trains/assets/complex_train.svg').set_color(WHITE).rotate(PI, UP)
 
-    self.play(Write(gnuSVG), run_time=3)
+    self.play(
+      Write(complexTrainSVG),
+      complexTrainSVG.animate.scale(0.25),
+      run_time=3
+    )
+
+    # Undo creation of the complex train and then animate the creation of a simple train.
+    # TODO: Make a class that handles the custom mobject creation of a train.
