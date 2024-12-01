28cd63b2e516b4a30c68e50680bc775ffe44872d
Donald Roman <romanunuose@gmail.com>
2024-12-01 22:12:24 +0100

    thought process for the creation of the custom mobjects

--- _2024YT/puzzle.py
+++ _2024YT/puzzle.py
@@ -11,5 +11,11 @@ class Puzzle(Scene):
       run_time=3
     )
 
+    # TODO: animate the creation of a train track and a background
+    # Not sure if this would be good enough in a 3D space or if it should be confined to 2D space.
+
+    # TODO: Make a class that handles the custom mobject creation of a fly.
+    # We can use SVGs to handle this. Should we be ambitious and animate the motion of the fly flapping its wings?
+
     # Undo creation of the complex train and then animate the creation of a simple train.
     # TODO: Make a class that handles the custom mobject creation of a train.
