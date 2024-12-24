4988caf6d51f0ec4c3d39bb99bef40963fb840a9
Donald Roman <romanunuose@gmail.com>
2024-12-24 22:26:10 +0100

    puzzle demo in the intro scene

--- _2024YT/puzzle.py
+++ _2024YT/puzzle.py
@@ -42,12 +42,12 @@ class Puzzle(MovingCameraScene):
   def construct(self):
     self.camera.frame.save_state()
 
-    # TODO: animate the creation of a train track and a background
-    # Not sure if this would be good enough in a 3D space or if it should be confined to 2D space.
+    #---------- Animate the creation of the required mobjects onto the scene. -----------#
     track = Line(start=LEFT*12, end=RIGHT*12).shift(DOWN*2)
 
     self.play(Write(track))
 
+    # Create the left train
     left_train = SimpleTrain(2)\
       .scale(0.5)\
       .next_to(track, direction=UP, buff=0.01)\
@@ -59,9 +59,7 @@ class Puzzle(MovingCameraScene):
       .next_to(track, direction=UP, buff=0.01)\
       .shift(RIGHT * 5)
 
-    # move the camera again
     self.play(
-      # self.camera.frame.animate.move_to(right_train),
       AnimationGroup(
         Write(left_train),
         Write(right_train),
@@ -69,14 +67,48 @@ class Puzzle(MovingCameraScene):
       )
     )
 
-    # Animate the creation of a complex train
-    gojo_fly = SVGMobject('./_2024YT/fly_2trains/assets/gojo_fly.svg').set_color(WHITE)
+    # Animate the creation of our fly, Gojo.
+    gojo_fly = SVGMobject('./_2024YT/fly_2trains/assets/gojo_fly.svg').set_color(WHITE).rotate(PI, axis=Y_AXIS)
     self.play(
       Write(gojo_fly),
-      gojo_fly.animate.scale(0.15).shift(DOWN*1.5),
+      gojo_fly.animate.scale(0.15).next_to(left_train, direction=RIGHT, buff=0.1),
       run_time=2
     )
 
+    # Make the fly oscillate at the starting position
+    self.play(
+      gojo_fly.animate.shift(UP * 0.125),
+     )
 
-    # TODO: Make a class that handles the custom mobject creation of a fly.
-    # We can use SVGs to handle this. Should we be ambitious and animate the motion of the fly flapping its wings?
+    # TODO: complete this simulation
+    # Simulate the movement of the trains and the fly (loop a few times)
+    def puzzle_demo():
+      self.play(
+        AnimationGroup(
+          left_train.animate(rate_func=linear).shift(RIGHT * 5 - np.array((left_train.width/2 + .03, 0.0, 0.0))),
+          right_train.animate(rate_func=linear).shift(LEFT * 5 + np.array((right_train.width/2 + .03, 0.0, 0.0))),
+          run_time=5,
+        )
+      )
+
+    puzzle_demo()
+    self.wait(0.2)
+
+    # Day2: TODO: Work on the Easy solution
+
+    # Day 3: TODO: Work on the Hard Solution
+
+    # Day 4: TODO: Record voiceover and work on half of the more complex solution
+
+    # Day 5: TODO: Work on the other half of the more complex solution
+
+    # Day 6: TODO: Finishihng touches and edit the animations
+
+    # Day 7: TODO: Edit it Adobe After EFfects
+
+
+    #---------------- EASY SOLUTION ----------------#
+    # Save the state of the fly and remove it from the scene
+    # gojo_fly.save_state()
+
+    # self.play(Unwrite(gojo_fly))
